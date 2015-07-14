1a7ea316fcd85120659cc6a75f5b472441709d82
CJ Hare <CjHare@users.noreply.github.com>
2015-07-14 21:25:24 +1000

    Adding the backtest project

---
+++ systematic-trading-backtest/src/main/java/com/systematic/trading/backtest/Backtest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2015, CJ Hare
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of [project] nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.systematic.trading.backtest;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.systematic.trading.data.DataPoint;
+import com.systematic.trading.data.DataService;
+import com.systematic.trading.data.DataServiceImpl;
+import com.systematic.trading.data.DataServiceUpdater;
+import com.systematic.trading.data.DataServiceUpdaterImpl;
+import com.systematic.trading.data.util.HibernateUtil;
+
+public class Backtest {
+
+    private static final Logger LOG = LogManager.getLogger(Backtest.class);
+
+    private static final int DAYS_IN_A_YEAR = 365;
+
+    /** Minimum amount of historical data needed for back testing. */
+    private static final int HISTORY_REQUIRED = 5 * DAYS_IN_A_YEAR;
+
+    public static void main(final String... args) {
+
+        final LocalDate endDate = LocalDate.now();
+        final LocalDate startDate = endDate.minus(HISTORY_REQUIRED, ChronoUnit.DAYS);
+
+        final String tickerSymbol = "^GSPC";
+        // final String tickerSymbol = "USD";
+
+        LOG.info(String.format("Including data set for %s from %s to %s", tickerSymbol, startDate, endDate));
+
+        final DataServiceUpdater updateService = DataServiceUpdaterImpl.getInstance();
+        updateService.get(tickerSymbol, startDate, endDate);
+
+        final DataService service = DataServiceImpl.getInstance();
+        final DataPoint[] data = service.get(tickerSymbol, startDate, endDate);
+
+        // TODO the actual back testing
+
+        // TODO buy triggers
+        // TODO sell triggers
+        // TODO money management
+        // TODO game loop - each cycle is over time, a trading day
+        // TODO metrics, time in / out market etc
+
+        HibernateUtil.getSessionFactory().close();
+    }
+}
