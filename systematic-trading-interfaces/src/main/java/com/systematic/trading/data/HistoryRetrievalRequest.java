4d10e8bc1cef887acd0f50ff66352347f46e0007
CJ Hare <CjHare@users.noreply.github.com>
2016-04-28 22:08:29 +1000

    Removing superfluous constructor

--- systematic-trading-interfaces/src/main/java/com/systematic/trading/data/HistoryRetrievalRequest.java
+++ systematic-trading-interfaces/src/main/java/com/systematic/trading/data/HistoryRetrievalRequest.java
@@ -60,9 +60,6 @@ public class HistoryRetrievalRequest implements Serializable {
 	@Column(name = "end", nullable = false)
 	private Date exclusiveEndDate;
 
-	public HistoryRetrievalRequest() {
-	}
-
 	public HistoryRetrievalRequest(final String tickerSymbol, final LocalDate inclusiveStartDate,
 	        final LocalDate exclusiveEndDate) {
 		this.tickerSymbol = tickerSymbol;
