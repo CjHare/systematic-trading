64746eed1b68c6d08f965374832c0367e9abb949
CJ Hare <CjHare@users.noreply.github.com>
2016-12-21 18:56:23 +1000

    Updating copyright years

--- systematic-trading-data/src/main/java/com/systematic/trading/data/DataServiceUpdaterImpl.java
+++ systematic-trading-data/src/main/java/com/systematic/trading/data/DataServiceUpdaterImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, CJ Hare All rights reserved.
+ * Copyright (c) 2015-2017-2017, CJ Hare All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met:
