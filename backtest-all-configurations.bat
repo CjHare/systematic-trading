9483a4e254dcce619881a2374b696d2997d2ef0c
CJ Hare <CjHare@users.noreply.github.com>
2017-06-28 00:20:16 +1000

    Updating launch parameters

--- backtest-all-configurations.bat
+++ backtest-all-configurations.bat
@@ -1,2 +1,2 @@
-call java -cp systematic-trading-backtest/target/systematic-trading-backtest-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.systematic.trading.backtest.AllConfigurations -output elastic-search -output_file_base_directory "../../simulations"
+call java -cp systematic-trading-backtest/target/systematic-trading-backtest-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.systematic.trading.backtest.AllConfigurations -output elastic_search -output_file_base_directory "../../simulations" -start_date 2007-06-01 -end_date 2017-06-01
 exit
