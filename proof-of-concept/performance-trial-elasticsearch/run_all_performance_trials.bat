ae463ed8a6dd12e6fcc11cace6da688508af12f0
CJ Hare <CjHare@users.noreply.github.com>
2017-08-23 01:27:41 +1000

    Tidying  up the batch file

--- proof-of-concept/performance-trial-elasticsearch/run_all_performance_trials.bat
+++ proof-of-concept/performance-trial-elasticsearch/run_all_performance_trials.bat
@@ -1,8 +1,11 @@
+REM @echo off
 REM Build the project & start the elastic search server before running the performance trials
-REM SET numberOfRecords = 125
-REM SET outputFile = "results/all_results.csv"
-REM call java -cp "target/elasticsearch-trial-performance.jar" com.systematic.trading.backtest.output.elastic.app.trial.ElasticSearchPerformanceTrialMultiThreadedSingleApi %numberOfRecords% %outputFile%
 
-call java -Xms1G -Xmx1G -cp "target/performance-trial-elasticsearch-0.0.1-SNAPSHOT-jar-with-dependencies.jar" com.systematic.trading.backtest.output.elastic.app.trial.ElasticSearchPerformanceTrialMultiThreadedSingleApi
+SET numberOfRecords=125
+SET outputFile="results/all_results.csv"
+SET library="target/performance-trial-elasticsearch-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+SET javaOptions=-Xms1G -Xmx1G
+
+call java %javaOptions% -cp %library% com.systematic.trading.backtest.output.elastic.app.trial.ElasticSearchPerformanceTrialMultiThreadedSingleApi %numberOfRecords% %outputFile%
 
 exit
