4a1a85bd5bc44ef02ecc47a65d48e735e85f292d
CJ Hare <CjHare@users.noreply.github.com>
2017-08-23 14:44:06 +1000

    Display, subroutine

--- proof-of-concept/performance-trial-elasticsearch/run_all_performance_trials.bat
+++ proof-of-concept/performance-trial-elasticsearch/run_all_performance_trials.bat
@@ -1,16 +1,37 @@
 @echo off
+SETLOCAL
 REM Build the project & start the elastic search server before running the performance trials
 
-SET numberOfRecords=250
+SET numberOfRecords=50
 SET outputFile="results/all_results.csv"
 SET library="target/performance-trial-elasticsearch-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 SET javaOptions=-Xms1G -Xmx1G
 SET classPrefix=com.systematic.trading.backtest.output.elastic.app.trial
 
-call java %javaOptions% -cp %library% %classPrefix%.ElasticSerialSearchPerformanceTrialSingleApi %numberOfRecords% %outputFile%
-call java %javaOptions% -cp %library% %classPrefix%.ElasticSearchSerialPerformanceTrialSingleApiNoReplicas %numberOfRecords% %outputFile%
-call java %javaOptions% -cp %library% %classPrefix%.ElasticSearchSerialPerformanceTrialSingleApiOneShard %numberOfRecords% %outputFile%
-call java %javaOptions% -cp %library% %classPrefix%.ElasticSearchParallelPerformanceTrialSingleApi %numberOfRecords% %outputFile%
-call java %javaOptions% -cp %library% %classPrefix%.ElasticSearchParallelPerformanceTrialManyThreadsSingleApi %numberOfRecords% %outputFile%
+echo ---------------------------------------------
+echo -------- Starting Performance Trials --------
+echo ---------------------------------------------
 
-exit
+call :runtTrial ElasticSerialSearchPerformanceTrialSingleApi
+call :runtTrial ElasticSearchSerialPerformanceTrialSingleApiNoReplicas
+call :runtTrial ElasticSearchSerialPerformanceTrialSingleApiOneShard
+call :runtTrial ElasticSearchParallelPerformanceTrialSingleApi
+call :runtTrial ElasticSearchParallelPerformanceTrialManyThreadsSingleApi
+
+echo ----------------------------------------------
+echo -------- Compelted Performance Trials -------- 
+echo ----------------------------------------------
+echo All results have been written to %outputFile%
+echo ----------------------------------
+
+rem main program must end with exit /b or goto :EOF
+exit /b
+
+
+
+rem ------ SUBROUTINES ------
+:runtTrial
+echo Starting: %1
+call java %javaOptions% -cp %library% %classPrefix%.%1 %numberOfRecords% %outputFile%
+echo Completed: %1
+exit /b
