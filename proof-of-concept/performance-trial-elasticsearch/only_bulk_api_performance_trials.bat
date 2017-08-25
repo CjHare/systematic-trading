971a8e9fc6b2c3990a66dae5e7af2a6a69134a3c
CJ Hare <CjHare@users.noreply.github.com>
2017-08-25 23:07:32 +1000

    Load testing

--- proof-of-concept/performance-trial-elasticsearch/only_bulk_api_performance_trials.bat
+++ proof-of-concept/performance-trial-elasticsearch/only_bulk_api_performance_trials.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 REM Build the project & start the elastic search server before running the performance trials
 
-SET numberOfRecords=12500
+SET numberOfRecords=3021500
 SET outputFile=results\only_bulk_api_trials.csv
 SET library="target/performance-trial-elasticsearch-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 SET javaOptions=-Xms1G -Xmx1G
@@ -15,7 +15,6 @@ echo ----------------------------------------------
 call :removeExistingOutputFile
 
 call :runtTrial ElasticSearchSerialPerformanceTrialBulkApi
-call :runtTrial ElasticSearchSerialPerformanceTrialBulkApiTinyPayload
 call :runtTrial ElasticSearchSerialPerformanceTrialBulkApiLargePayload
 call :runtTrial ElasticSearchSerialPerformanceTrialBulkApiRefreshDisabled
 call :runtTrial ElasticSearchSerialPerformanceTrialBulkApiMetaContainsIndex
@@ -24,6 +23,9 @@ call :runtTrial ElasticSearchSerialPerformanceTrialBulkApiMetaContainsIndexType
 call :runtTrial ElasticSearchParallelPerformanceTrialBulkApi
 call :runtTrial ElasticSearchParallelPerformanceTrialBulkApiManyThreads
 
+
+REM This test is way too slow ~10x longer then the base line ElasticSearchSerialPerformanceTrialBulkApi
+REM call :runtTrial ElasticSearchSerialPerformanceTrialBulkApiTinyPayload
 
 echo ----------------------------------------------
 echo All results written to %outputFile%
