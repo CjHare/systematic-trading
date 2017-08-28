dc6a9884022e86a61af08a7d6ff0ec91e2549e36
CJ Hare <CjHare@users.noreply.github.com>
2017-08-28 21:24:05 +1000

    Greater number of runs

--- proof-of-concept/performance-trial-elasticsearch/optimal_bulk_api_performance_trial.bat
+++ proof-of-concept/performance-trial-elasticsearch/optimal_bulk_api_performance_trial.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 REM Build the project & start the elastic search server before running the performance trials
 
-SET numberOfRecords=3021500
+SET numberOfRecords=5021250
 SET outputFile=results\optimal_bulk_api_trial.csv
 SET library="target/performance-trial-elasticsearch-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 SET javaOptions=-Xms1G -Xmx1G
