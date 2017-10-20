ffb787b0e1e3c140274848e51b3fcbd35f8ebe0e
CJ Hare <CjHare@users.noreply.github.com>
2017-10-20 22:15:35 +1000

    Rewriting UT for absolute values and dates

--- systematic-trading-maths/src/test/java/com/systematic/trading/maths/indicator/ema/ClosingPriceExponentialMovingAverageCalculatorTest.java
+++ systematic-trading-maths/src/test/java/com/systematic/trading/maths/indicator/ema/ClosingPriceExponentialMovingAverageCalculatorTest.java
@@ -25,13 +25,20 @@
  */
 package com.systematic.trading.maths.indicator.ema;
 
+import static com.systematic.trading.maths.util.SystematicTradingMathsAssert.assertValues;
+import static com.systematic.trading.maths.util.SystematicTradingMathsAssert.line;
+import static com.systematic.trading.maths.util.SystematicTradingMathsAssert.point;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.SortedMap;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -56,50 +63,93 @@ public class ClosingPriceExponentialMovingAverageCalculatorTest {
 	/** Calculator instance being tested. */
 	private ExponentialMovingAverageIndicator calculator;
 
-	@Test
-	public void emaOnePoints() {
-		final int lookback = 2;
-		final TradingDayPrices[] data = createPrices(lookback);
-		setUpCalculator(lookback);
-
-		final ExponentialMovingAverageLine ema = ema(data);
-
-		verifyEma(ema, 1);
-		verifyValidation(data, lookback);
-	}
-
-	@Test
-	public void emaTwoPoints() {
-		final int lookback = 2;
-		final TradingDayPrices[] data = createPrices(3);
-		setUpCalculator(lookback);
-
-		final ExponentialMovingAverageLine ema = ema(data);
-
-		verifyEma(ema, 1, 1);
-		verifyValidation(data, lookback);
-	}
-
-	@Test
-	public void emaThreePoints() {
-		final int lookback = 2;
-		final TradingDayPrices[] data = createIncreasingPrices(4);
-		setUpCalculator(lookback);
-
-		final ExponentialMovingAverageLine ema = ema(data);
-
-		verifyEma(ema, 1.5, 2.5, 3.5);
-		verifyValidation(data, lookback);
+	@Test(expected = IllegalArgumentException.class)
+	public void invalidLookback() {
+		setUpValidationErrorGreaterThan();
+		setUpCalculator(0);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void emaTwoPointsLastNull() {
-		final TradingDayPrices[] data = createIncreasingPrices(4);
-		data[data.length - 1] = null;
-		setUpValidationErrorZeroEntries();
+	public void invalidDaysOfEmaValues() {
+		setUpValidationErrorGreaterThan();
+		setUpCalculator(1, 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void emaNullInput() {
+		setUpValidationErrorNullInput();
+		setUpCalculator(1);
+
+		ema(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void emaNullEntries() {
+		setUpValidationErrorNullEntries();
 		setUpCalculator(2);
 
-		ema(data);
+		ema(new TradingDayPrices[] { null });
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void emaNotEnoughValues() {
+		setUpValidationErrorNullInput();
+		setUpCalculator(1);
+
+		ema(new TradingDayPrices[] {});
+	}
+
+	@Test
+	public void emaIntelExample() {
+		final int lookback = 10;
+		final TradingDayPrices[] data = createIntelExamplePrices();
+		setUpCalculator(lookback);
+
+		final ExponentialMovingAverageLine ema = ema(data);
+
+		verifyEma(ema,
+		        line(point(LocalDate.of(2010, 4, 7), 22.22), point(LocalDate.of(2010, 4, 8), 22.21),
+		                point(LocalDate.of(2010, 4, 9), 22.24), point(LocalDate.of(2010, 4, 12), 22.27),
+		                point(LocalDate.of(2010, 4, 13), 22.33), point(LocalDate.of(2010, 4, 14), 22.52),
+		                point(LocalDate.of(2010, 4, 15), 22.80), point(LocalDate.of(2010, 4, 16), 22.97),
+		                point(LocalDate.of(2010, 4, 19), 23.13), point(LocalDate.of(2010, 4, 20), 23.28),
+		                point(LocalDate.of(2010, 4, 21), 23.34), point(LocalDate.of(2010, 4, 22), 23.43),
+		                point(LocalDate.of(2010, 4, 23), 23.51), point(LocalDate.of(2010, 4, 26), 23.53),
+		                point(LocalDate.of(2010, 4, 27), 23.47), point(LocalDate.of(2010, 4, 28), 23.40),
+		                point(LocalDate.of(2010, 4, 29), 23.39), point(LocalDate.of(2010, 4, 30), 23.26),
+		                point(LocalDate.of(2010, 5, 3), 23.23), point(LocalDate.of(2010, 5, 4), 23.08),
+		                point(LocalDate.of(2010, 5, 5), 22.92)));
+		verifyValidation(data, lookback);
+	}
+
+	@Test
+	public void emaIncreasing() {
+		final int lookback = 5;
+		final TradingDayPrices[] data = createIncreasingPrices();
+		setUpCalculator(lookback);
+
+		final ExponentialMovingAverageLine ema = ema(data);
+
+		verifyEma(ema,
+		        line(point(LocalDate.of(2017, 9, 15), 2), point(LocalDate.of(2017, 9, 18), 3),
+		                point(LocalDate.of(2017, 9, 19), 4), point(LocalDate.of(2017, 9, 20), 5),
+		                point(LocalDate.of(2017, 9, 21), 6), point(LocalDate.of(2017, 9, 22), 7)));
+		verifyValidation(data, lookback);
+	}
+
+	@Test
+	public void emaFlat() {
+		final int lookback = 4;
+		final TradingDayPrices[] data = createFlatPrices();
+		setUpCalculator(lookback);
+
+		final ExponentialMovingAverageLine ema = ema(data);
+
+		verifyEma(ema,
+		        line(point(LocalDate.of(2017, 10, 12), 4.5), point(LocalDate.of(2017, 5, 13), 4.5),
+		                point(LocalDate.of(2017, 10, 16), 4.5), point(LocalDate.of(2017, 10, 17), 4.5),
+		                point(LocalDate.of(2017, 10, 18), 4.5)));
+		verifyValidation(data, lookback);
 	}
 
 	@Test
@@ -111,56 +161,37 @@ public class ClosingPriceExponentialMovingAverageCalculatorTest {
 		assertEquals(9, requiredDays);
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void emaNullInputArray() {
-		setUpValidationErrorNullInput();
-		setUpCalculator(1);
-		final TradingDayPrices[] input = null;
-
-		ema(input);
-	}
-
-	@Test
-	/**
-	 * Ten day EMA, with Intel price data from 24-Mar-10 to 5-May-10
-	 */
-	public void emaArrayIntelExample() {
-		final int lookback = 10;
-		final TradingDayPrices[] data = createPrices(22.27, 22.19, 22.08, 22.17, 22.18, 22.13, 22.23, 22.43, 22.24,
-		        22.29, 22.15, 22.39, 22.38, 22.61, 23.36, 24.05, 23.75, 23.83, 23.95, 23.63, 23.82, 23.87, 23.65, 23.19,
-		        23.10, 23.33, 22.68, 23.10, 22.40, 22.17);
-		setUpCalculator(lookback);
-
-		final ExponentialMovingAverageLine ema = ema(data);
-
-		verifyEma(ema, 22.22, 22.21, 22.24, 22.27, 22.33, 22.52, 22.80, 22.97, 23.13, 23.28, 23.34, 23.43, 23.51, 23.53,
-		        23.47, 23.40, 23.39, 23.26, 23.23, 23.08, 22.92);
-		verifyValidation(data, lookback);
-	}
-
 	private ExponentialMovingAverageLine ema( final TradingDayPrices[] data ) {
 		return calculator.calculate(data);
+	}
+
+	private void setUpValidationErrorGreaterThan() {
+		doThrow(new IllegalArgumentException()).when(validator).verifyGreaterThan(anyInt(), eq(0));
 	}
 
 	private void setUpValidationErrorNullInput() {
 		doThrow(new IllegalArgumentException()).when(validator).verifyNotNull(any());
 	}
 
-	private void setUpValidationErrorZeroEntries() {
+	private void setUpValidationErrorNullEntries() {
 		doThrow(new IllegalArgumentException()).when(validator).verifyZeroNullEntries(any(TradingDayPrices[].class));
 	}
 
-	private void verifyEma( final ExponentialMovingAverageLine actual, final double... expected ) {
+	private void verifyEma( final ExponentialMovingAverageLine actual,
+	        final SortedMap<LocalDate, BigDecimal> expected ) {
 		assertNotNull(actual);
 		assertNotNull(actual.getEma());
-		assertEquals(expected.length, actual.getEma().size());
+		assertEquals(expected.size(), actual.getEma().size());
+		assertValues(expected, actual.getEma());
 
-		//TODO code
-		//		assertValues(expected, actual.getEma());
 	}
 
 	private void setUpCalculator( final int lookback ) {
 		calculator = new ClosingPriceExponentialMovingAverageCalculator(lookback, 1, validator);
+	}
+
+	private void setUpCalculator( final int lookback, final int daysOfEmaValues ) {
+		calculator = new ClosingPriceExponentialMovingAverageCalculator(lookback, daysOfEmaValues, validator);
 	}
 
 	private void verifyValidation( final TradingDayPrices[] data, final int lookback ) {
@@ -170,36 +201,60 @@ public class ClosingPriceExponentialMovingAverageCalculatorTest {
 		verify(validator).verifyZeroNullEntries(data);
 	}
 
-	private TradingDayPrices[] createPrices( final int count ) {
-		final TradingDayPrices[] prices = new TradingDayPrices[count];
+	/**
+	 * Flat prices starting from LocalDate.of(2017, 10, 9).
+	 */
+	private TradingDayPrices[] createFlatPrices() {
+		final LocalDate[] dates = { LocalDate.of(2017, 10, 9), LocalDate.of(2017, 10, 10), LocalDate.of(2017, 10, 11),
+		        LocalDate.of(2017, 10, 12), LocalDate.of(2017, 5, 13), LocalDate.of(2017, 10, 16),
+		        LocalDate.of(2017, 10, 17), LocalDate.of(2017, 10, 18) };
+		final double[] close = { 4.5, 4.5, 4.5, 4.5, 4.5, 4.5, 4.5, 4.5 };
 
-		for (int i = 0; i < count; i++) {
-			prices[i] = new TradingDayPricesBuilder().withTradingDate(LocalDate.now().plusDays(i)).withOpeningPrice(1)
-			        .withLowestPrice(0).withHighestPrice(2).withClosingPrice(1).build();
-		}
-
-		return prices;
+		return createPrices(dates, close);
 	}
 
-	private TradingDayPrices[] createPrices( final double... values ) {
-		final TradingDayPrices[] prices = new TradingDayPrices[values.length];
+	/**
+	 * Increasing prices starting from LocalDate.of(2017, 9, 11).
+	 */
+	private TradingDayPrices[] createIncreasingPrices() {
+		final LocalDate[] dates = { LocalDate.of(2017, 9, 11), LocalDate.of(2017, 9, 12), LocalDate.of(2017, 9, 13),
+		        LocalDate.of(2017, 9, 14), LocalDate.of(2017, 9, 15), LocalDate.of(2017, 9, 18),
+		        LocalDate.of(2017, 9, 19), LocalDate.of(2017, 9, 20), LocalDate.of(2017, 9, 21),
+		        LocalDate.of(2017, 9, 22) };
+		final double[] close = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-		for (int i = 0; i < values.length; i++) {
-			prices[i] = new TradingDayPricesBuilder().withTradingDate(LocalDate.now().plusDays(i)).withOpeningPrice(1)
-			        .withLowestPrice(0).withHighestPrice(2).withClosingPrice(values[i]).build();
-		}
-
-		return prices;
+		return createPrices(dates, close);
 	}
 
-	private TradingDayPrices[] createIncreasingPrices( final int count ) {
-		final TradingDayPrices[] prices = new TradingDayPrices[count];
+	/**
+	 * Thirty days of price data for Intel starting from LocalDate.of(2010, 3, 24).
+	 */
+	private TradingDayPrices[] createIntelExamplePrices() {
+		final LocalDate[] dates = { LocalDate.of(2010, 3, 24), LocalDate.of(2010, 3, 25), LocalDate.of(2010, 3, 26),
+		        LocalDate.of(2010, 3, 29), LocalDate.of(2010, 3, 30), LocalDate.of(2010, 3, 31),
+		        LocalDate.of(2010, 4, 1), LocalDate.of(2010, 4, 5), LocalDate.of(2010, 4, 6), LocalDate.of(2010, 4, 7),
+		        LocalDate.of(2010, 4, 8), LocalDate.of(2010, 4, 9), LocalDate.of(2010, 4, 12),
+		        LocalDate.of(2010, 4, 13), LocalDate.of(2010, 4, 14), LocalDate.of(2010, 4, 15),
+		        LocalDate.of(2010, 4, 16), LocalDate.of(2010, 4, 19), LocalDate.of(2010, 4, 20),
+		        LocalDate.of(2010, 4, 21), LocalDate.of(2010, 4, 22), LocalDate.of(2010, 4, 23),
+		        LocalDate.of(2010, 4, 26), LocalDate.of(2010, 4, 27), LocalDate.of(2010, 4, 28),
+		        LocalDate.of(2010, 4, 29), LocalDate.of(2010, 4, 30), LocalDate.of(2010, 5, 3),
+		        LocalDate.of(2010, 5, 4), LocalDate.of(2010, 5, 5) };
+		final double[] close = { 22.27, 22.19, 22.08, 22.17, 22.18, 22.13, 22.23, 22.43, 22.24, 22.29, 22.15, 22.39,
+		        22.38, 22.61, 23.36, 24.05, 23.75, 23.83, 23.95, 23.63, 23.82, 23.87, 23.65, 23.19, 23.10, 23.33, 22.68,
+		        23.10, 22.40, 22.17 };
 
-		for (int i = 0; i < count; i++) {
-			prices[i] = new TradingDayPricesBuilder().withTradingDate(LocalDate.now().plusDays(i))
-			        .withOpeningPrice(i + 1).withLowestPrice(1).withHighestPrice(i + 2).withClosingPrice(i + 1).build();
+		return createPrices(dates, close);
+	}
+
+	private TradingDayPrices[] createPrices( final LocalDate[] dates, final double[] close ) {
+		final TradingDayPrices[] data = new TradingDayPrices[dates.length];
+
+		// Only the close price is used in the EMA calculation
+		for (int i = 0; i < data.length; i++) {
+			data[i] = new TradingDayPricesBuilder().withTradingDate(dates[i]).withClosingPrice(close[i]).build();
 		}
 
-		return prices;
+		return data;
 	}
 }
