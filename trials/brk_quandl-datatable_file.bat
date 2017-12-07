1574bfe9335de4cd30f33790d169601fc46e173b
CJ Hare <CjHare@users.noreply.github.com>
2017-12-07 20:05:08 +1000

    amending the bat files

---
+++ trials/brk_quandl-datatable_file.bat
@@ -0,0 +1,2 @@
+call trial.bat -output file_complete -start_date 2007-06-01 -end_date 2017-06-01 -data_service_type "tables" -equity_dataset "WIKI" -ticker_symbol "BRK_A" -output_file_base_directory "../../results"
+exit
--- trials/trial_quandl-dataset_file.bat
+++
@@ -1,2 +0,0 @@
-call trial.bat -output file_complete -start_date 2007-06-01 -end_date 2017-06-01 -data_service_type "time-series" -equity_dataset "OPEC" -ticker_symbol "ORB" -output_file_base_directory "../results"
-exit
