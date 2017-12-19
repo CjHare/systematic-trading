3ba833391c0f6c8f7cb70bb6c70b184630d5fd0a
CJ Hare <CjHare@users.noreply.github.com>
2017-12-19 18:59:58 +1000

    Removing unused import

--- systematic-trading-strategy/src/test/java/com/systematic/trading/strategy/TradingStrategyTest.java
+++ systematic-trading-strategy/src/test/java/com/systematic/trading/strategy/TradingStrategyTest.java
@@ -31,7 +31,6 @@ package com.systematic.trading.strategy;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
