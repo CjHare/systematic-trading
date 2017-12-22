d06d122deda0a47d9cf8406b33c3ab7d02df8885
CJ Hare <CjHare@users.noreply.github.com>
2017-12-22 13:55:13 +1000

    Adding dependencies back into backtest to eliminate code duplication

--- systematic-trading-backtest-trial/src/main/java/com/systematic/trading/backtest/BacktestTrial.java
+++ systematic-trading-backtest-trial/src/main/java/com/systematic/trading/backtest/BacktestTrial.java
@@ -42,6 +42,7 @@ import com.systematic.trading.backtest.configuration.BacktestBootstrapConfigurat
 import com.systematic.trading.backtest.configuration.deposit.DepositConfiguration;
 import com.systematic.trading.backtest.configuration.equity.EquityConfiguration;
 import com.systematic.trading.backtest.context.BacktestBootstrapContext;
+import com.systematic.trading.backtest.context.BacktestBootstrapContextBulider;
 import com.systematic.trading.backtest.description.DescriptionGenerator;
 import com.systematic.trading.backtest.description.StandardDescriptionGenerator;
 import com.systematic.trading.backtest.event.BacktestEventListener;
