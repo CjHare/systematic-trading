73136cad90183aad8776dac11ede36a33993a846
CJ Hare <CjHare@users.noreply.github.com>
2017-12-30 17:22:10 +1000

    Delete brk_quandl-datatable_es.bat

--- scripts/backtest/brk_quandl-datatable_es.bat
+++
@@ -1,2 +0,0 @@
-call backtest.bat -output elastic_search -start_date 2007-06-01 -end_date 2017-06-01 -data_service_type "tables" -equity_dataset "WIKI" -ticker_symbol "BRK_A" -opening_funds 0 -deposit_amount 200 -deposit_frequency WEEKLY
-exit
