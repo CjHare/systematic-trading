315894942e00e136e5527787d6ff5a9ea8c0565e
CJ Hare <CjHare@users.noreply.github.com>
2017-12-30 16:58:11 +1000

    Delete msci_world.bat

--- scripts/analysis/msci_world.bat
+++
@@ -1,3 +0,0 @@
-REM London Stock Exchange (LSE) : SRCE MSCI WORLD ETF price (MXWO), Currency USD
-call analysis.bat -data_service_type "time-series" -equity_dataset "LSE" -ticker_symbol "MXWO" -opening_funds 10000
-exit
