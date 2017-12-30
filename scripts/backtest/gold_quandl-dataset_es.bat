51e59a035d3721cc7c01db2051074775b9b7f88b
CJ Hare <CjHare@users.noreply.github.com>
2017-12-30 16:59:01 +1000

    Delete gold_quandl-dataset_es.bat

--- scripts/backtest/gold_quandl-dataset_es.bat
+++
@@ -1,2 +0,0 @@
-call backtest.bat -output elastic_search -start_date 2007-06-01 -end_date 2017-06-01 -data_service_type "time-series" -equity_dataset "LBMA" -ticker_symbol "GOLD" -opening_funds 0 -deposit_amount 200 -deposit_frequency WEEKLY
-exit
