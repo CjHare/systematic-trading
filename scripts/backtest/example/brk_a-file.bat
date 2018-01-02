a5bf63427b85f261db0aeddb9cf48b2989d0ae2c
CJ Hare <CjHare@users.noreply.github.com>
2018-01-02 14:43:49 +1000

    Removing the exit, caused problem under default windows install with UAC

--- scripts/backtest/example/brk_a-file.bat
+++ scripts/backtest/example/brk_a-file.bat
@@ -1,2 +1,1 @@
 call backtest.bat -output file_complete -start_date 2007-06-01 -end_date 2017-06-01 -data_service_type "tables" -equity_dataset "WIKI" -ticker_symbol "BRK_A" -output_file_base_directory "../../../../results" -opening_funds 0 -deposit_amount 200 -deposit_frequency WEEKLY
-exit
