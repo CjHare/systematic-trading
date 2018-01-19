ce0ab8598b61b8c2bb3d7cc278c32131f0c0a64a
cjhare <CjHare@users.noreply.github.com>
2018-01-19 13:22:19 +1000

    Updating year in licence

--- systematic-trading-simulation-model/src/main/java/com/systematic/trading/simulation/brokerage/Brokerage.java
+++ systematic-trading-simulation-model/src/main/java/com/systematic/trading/simulation/brokerage/Brokerage.java
@@ -1,6 +1,6 @@
 /**
  * 
- * Copyright (c) 2015-2017, CJ Hare All rights reserved.
+ * Copyright (c) 2015-2018, CJ Hare All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met:
